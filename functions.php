d7aa7d548505bef047137bffffe350bcb3972c99
Joel Yoder <github@joelyoder.com>
2021-09-06 00:28:45 -0400

    Intial commit

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: MTE CPT Intervention
+ * Description: Registers an "intervention" custom post type for ProCamp.
+ * Author: Joel Yoder
+ * Author URI: https://joelyoder.com
+ * Version: 1.0.0
+ * GitHub Plugin URI: https://github.com/joelyoder/mte-cpt-intervention
+ * GitHub Branch: main
+ */
